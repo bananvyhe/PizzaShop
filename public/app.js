fbdef2c0c34d2acace828cc1870d09ce361afe5a
bananvyhe <loadonden@yahoo.com>
2016-05-22 13:39:26 +0600

    add cart func on Js localStorage

--- public/app.js
+++ public/app.js
@@ -8,3 +8,10 @@ function something()
 	alert(x);
 	
 }
+
+function add_to_cart(id)
+{
+	var x = window.localStorage.getItem('product_' + id);
+	x=x*1+1;
+	window.localStorage.setItem('product_' + id, x);
+}
