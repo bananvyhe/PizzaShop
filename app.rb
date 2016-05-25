a62cc9d0d3112a84cc1206d3c3145bb753251a55
bananvyhe <loadonden@yahoo.com>
2016-05-25 17:20:59 +0600

    add cart view

--- app.rb
+++ app.rb
@@ -9,11 +9,8 @@ set :database, "sqlite3:pizzashop.db"
 class Product < ActiveRecord::Base
 end
 
-before do
-	@products= Product.all
-end
-
 get '/' do
+	@products = Product.all
 	erb :index
 end
 
@@ -24,4 +21,32 @@ end
 get '/products' do
 	@products = Product.order('created_at DESC')
 	erb :products
+end
+
+
+
+post '/cart' do
+	 orders_input = params[:orders]
+	 @items = parse_orders_input orders_input
+	 @items.each do |item|
+	 	item[0] = Product.find(item[0])
+	 end
+	erb :cart
+end
+
+def parse_orders_input orders_input
+	s1 = orders_input.split(/,/)
+	arr = []
+
+	s1.each do |x|
+		s2 = x.split(/\=/)
+		s3 = s2[0].split(/_/)
+
+		id = s3[1]
+		cnt = s2[1]
+
+		arr2 = [id, cnt]
+		arr.push arr2
+	end	
+	return arr
 end
